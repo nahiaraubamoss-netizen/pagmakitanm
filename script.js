afb98da6edc404e3cfe06432536ededd28ec3e30
Nahiara Mosquera <Nahiara@Nahiara.local>
2026-06-25 19:04:42 -0300

    Subir pagina Makita

---
+++ script.js
@@ -0,0 +1,33 @@
+const colorSwap = document.querySelector(".color-swap");
+const greenDot = document.querySelector(".color-dot-green");
+const redDot = document.querySelector(".color-dot-red");
+const blueDot = document.querySelector(".color-dot-blue");
+const colorDots = [blueDot, redDot, greenDot];
+
+const setActiveDot = (dot) => {
+  colorDots.forEach((item) => item?.classList.toggle("is-active", item === dot));
+};
+
+setActiveDot(redDot);
+
+greenDot?.addEventListener("click", () => {
+  colorSwap?.classList.add("is-green-active");
+  colorSwap?.classList.remove("is-blue-active");
+  setActiveDot(greenDot);
+});
+
+redDot?.addEventListener("click", () => {
+  colorSwap?.classList.add("is-resetting");
+  colorSwap?.classList.remove("is-green-active");
+  colorSwap?.classList.remove("is-blue-active");
+  setActiveDot(redDot);
+  requestAnimationFrame(() => {
+    colorSwap?.classList.remove("is-resetting");
+  });
+});
+
+blueDot?.addEventListener("click", () => {
+  colorSwap?.classList.add("is-blue-active");
+  colorSwap?.classList.remove("is-green-active");
+  setActiveDot(blueDot);
+});
